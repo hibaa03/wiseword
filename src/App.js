ba5ebe3ab5793c7618757b7c6a51fb1922d3f470
hiba <114129261+lanaaman@users.noreply.github.com>
2023-06-04 06:57:52 +0500

    App.js React updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react';
+import Alert from './components/Alert';
+import {
+BrowserRouter,
+Routes,
+Route,
+} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [mode, setMode] = useState('light'); // whether darkmode is enabled or not
+  const[alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+  })
+  setTimeout(()=> {
+    setAlert(null);
+  }, 3000)
+  }
+  
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("dark mode has been enabled", "success")
+      document.title = 'TextUtils - Dark Mode'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+      showAlert("light mode has been enabled", "success")
+      // document.title = 'TextUtils - Light Mode'
+    }
+   }
+  
+   return (
+    <>
+    <BrowserRouter>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className='container my-3'>
+    <Routes>
+    <Route path="/about" element={<About mode={mode}/>} />
+          <Route path="/" element= {<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/> } />
+    </Routes>
     </div>
+    </BrowserRouter>
+    </>
   );
 }
-
 export default App;
